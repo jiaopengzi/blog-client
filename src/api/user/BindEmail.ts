2cee08f91142f401926282fc383d79dc18616c05
jiaopengzi <jiaopengzi@outlook.com>
2023-10-22 20:48:31 +0800

    绑定邮箱弹窗

---
+++ src/api/user/BindEmail.ts
@@ -0,0 +1,35 @@
+/**
+ * @Author       : jiaopengzi
+ * @Date         : 2023-10-21 20:26:33
+ * @LastEditors  : jiaopengzi
+ * @LastEditTime : 2023-10-21 20:27:56
+ * @FilePath     : \blog-client\src\api\user\BindEmail.ts
+ * @Description  : 绑定邮箱
+ * @Blog         : https://jiaopengzi.com
+ * @Copyright    : Copyright (c) 2023 by jiaopengzi, All Rights Reserved.
+ */
+
+import request from '../request'
+import { routerGroup } from '../routerGroup'
+import type { AxiosPromise } from 'axios'
+
+export interface BindEmailRequest {
+  email: string
+  captcha: string
+}
+
+export interface BindEmailResponse {
+  code: number
+  msg: string
+  data: any // 您可以根据实际返回的数据结构替换为更具体的类型
+}
+
+// 注册
+export function bindEmailByJosn(requestData: string): AxiosPromise<BindEmailResponse> {
+  const urlStr = routerGroup + '/user/bind/email'
+  return request({
+    url: urlStr,
+    method: 'post',
+    data: requestData,
+  })
+}
