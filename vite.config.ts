6ad13214e1fb4f474ce3957879621491cf279bcf
jiaopengzi <jiaopengzi@outlook.com>
2024-09-13 12:41:05 +0800

    使用path

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@
  * @Blog         : https://jiaopengzi.com
  * @Copyright    : Copyright (c) 2023 by jiaopengzi, All Rights Reserved.
  */
-import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -37,7 +37,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // src 为根目录
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 
