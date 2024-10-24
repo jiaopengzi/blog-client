08cb989570f9736d10126722e667800830a2c454
jiaopengzi <jiaopengzi@outlook.com>
2024-10-24 18:50:20 +0800

    邮箱反向代理配置

--- scripts/openDocument.js
+++ scripts/openDocument.js
@@ -2,30 +2,29 @@
  * @Author       : jiaopengzi
  * @Date         : 2024-01-13 13:36:09
  * @LastEditors  : jiaopengzi
- * @LastEditTime : 2024-09-10 14:51:05
+ * @LastEditTime : 2024-10-24 18:48:50
  * @FilePath     : \blog-client\scripts\openDocument.js
- * @Description  : 打开指定的文档 
+ * @Description  : 打开指定的文档
  * @Blog         : https://jiaopengzi.com
- * @Copyright    : Copyright (c) 2024 by jiaopengzi, All Rights Reserved. 
+ * @Copyright    : Copyright (c) 2024 by jiaopengzi, All Rights Reserved.
  */
 
-
-import child_process from 'child_process'
+import child_process from "child_process"
 
 // const url = 'https://jiaopengzi.com'
-const url1 = 'http://10.10.2.222:8081'
-let cmd = ''
+const url1 = "http://10.10.2.222:7364"
+let cmd = ""
 // console.log(process.platform)
 switch (process.platform) {
-  case 'win32':
-    cmd = 'start'
-    // child_process.exec(cmd + ' ' + url)
-    child_process.exec(cmd + ' ' + url1)
-    break
+    case "win32":
+        cmd = "start"
+        // child_process.exec(cmd + ' ' + url)
+        child_process.exec(cmd + " " + url1)
+        break
 
-  case 'darwin':
-    cmd = 'open'
-    // child_process.exec(cmd + ' ' + url)
-    child_process.exec(cmd + ' ' + url1)
-    break
+    case "darwin":
+        cmd = "open"
+        // child_process.exec(cmd + ' ' + url)
+        child_process.exec(cmd + " " + url1)
+        break
 }
