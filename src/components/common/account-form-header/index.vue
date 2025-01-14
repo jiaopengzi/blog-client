fc41aec1b6524305a2bbb1d4439f5fb7a7b17483
jiaopengzi <jiaopengzi@outlook.com>
2025-01-14 16:02:36 +0800

    图片正确显示02

--- src/components/common/account-form-header/index.vue
+++ src/components/common/account-form-header/index.vue
@@ -2,7 +2,7 @@
  * @Author       : jiaopengzi
  * @Date         : 2025-01-13 10:19:16
  * @LastEditors  : jiaopengzi
- * @LastEditTime : 2025-01-14 15:38:37
+ * @LastEditTime : 2025-01-14 16:02:02
  * @FilePath     : \blog-client\src\components\common\account-form-header\index.vue
  * @Description  : 账号相关表单头部
  * @Blog         : https://jiaopengzi.com
@@ -15,7 +15,7 @@
         <div v-if="aTag" class="header-main">
             <a :href="aTag.href" :target="aTag.target">
                 <div class="logo">
-                    <img :src="imgSrc" :alt="alt" />
+                    <img src="@/assets/img/logo-text-rounded-rectangle-200-52.png" :alt="alt" />
                 </div>
             </a>
             <h2 v-if="title" class="title">{{ title }}</h2>
@@ -25,7 +25,7 @@
         <div v-if="routerLinkTo" class="header-main">
             <router-link v-if="routerLinkTo" :to="routerLinkTo" class="link">
                 <div class="logo">
-                    <img :src="imgSrc" :alt="alt" />
+                    <img src="@/assets/img/logo-text-rounded-rectangle-200-52.png" :alt="alt" />
                 </div>
             </router-link>
             <h2 v-if="title" class="title">{{ title }}</h2>
@@ -36,8 +36,6 @@
 <script lang="ts" setup>
 import type { RouteLocationAsPathGeneric, RouteLocationAsRelativeGeneric } from "vue-router"
 
-import logo from "@/assets/img/logo-text-rounded-rectangle-200-52.png"
-
 defineOptions({ name: "AccountFormHeader" })
 
 const {
@@ -45,7 +43,7 @@ const {
     alt = "www.jiaopengzi.com",
     // 注意: Vite 的 import.meta.url 结合 new URL() 方法来解析图片路径。确保在开发和生产环境中都能正确解析路径。
     // imgSrc = new URL("@/assets/img/logo-text-rounded-rectangle-200-52.png", import.meta.url).href,
-    imgSrc = logo,
+    imgSrc,
     aTag,
     routerLinkTo,
 } = defineProps<{
