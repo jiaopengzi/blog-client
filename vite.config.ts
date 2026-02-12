d3ec454485738d9db90d7229812a8996ab817101
jiaopengzi <jiaopengzi@outlook.com>
2026-02-12 20:14:09 +0800

    css 使用标准的 if

--- vite.config.ts
+++ vite.config.ts
@@ -116,6 +116,7 @@ export default defineConfig({
                     @use '@/assets/scss/platform/_pc.scss' as pc;
                     @use '@/assets/scss/mixin.scss' as *;
                     `,
+                silenceDeprecations: ["if-function"],
             },
             // devSourceMap: true, // 开发环境下是否生成 sourceMap
         },
