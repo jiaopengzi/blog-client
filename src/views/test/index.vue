c862bdea5b92c2c51df9a8fbc4eb39eb739ffe1d
jiaopengzi <jiaopengzi@outlook.com>
2026-05-09 09:24:00 +0800

    📝 docs: 更新 test 页面

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -3,18 +3,39 @@
  * Description : 
 -->
 <template>
-    <JEditor :state-manager="stateManager" :is-enable-copy-cache="false" />
-
-    <FooterStatistics v-if="optionsStore.isShowFooterStatistics" />
+    <div class="test">This is a test page.</div>
+    <div class="go-home">
+        <router-link to="/">返回首页</router-link>
+    </div>
 </template>
 <script lang="ts" setup>
-import { EditorStateManager } from "@/components/editor"
-import FooterStatistics from "@/components/layout/footer-statistics"
-import { useOptionsStore } from "@/stores/options"
-import JEditor from "@/components/editor/index.vue"
-
 defineOptions({ name: "DemoTest" })
-
-const optionsStore = useOptionsStore()
-const stateManager = new EditorStateManager()
 </script>
+<style scoped lang="scss">
+.test {
+    font-size: 24px;
+    color: var(--jpz-text-color-primary);
+    text-align: center;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    padding: 20px;
+    //border: 1px solid #ccc;
+    //border-radius: 8px;
+    //background-color: #f9f9f9;
+    //box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    // transition: all 0.3s ease;
+}
+
+.go-home {
+    text-align: center;
+    margin-top: 20px;
+    a {
+        font-size: 18px;
+        color: var(--jpz-text-color-primary);
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+}
+</style>
